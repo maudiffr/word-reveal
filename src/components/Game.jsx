13f035d35c6667fd39595d85e0e299acd6ddbe0b
manu <159138403+maudiffr@users.noreply.github.com>
2026-03-23 19:56:39 +0000

    Modify word timer

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -10,7 +10,7 @@ const Game = () => {
   const [revealedLetters, setRevealedLetters] = useState([])
 
   const startGame = () => {
-    let newWord = 'BUT'
+    let newWord = getRandomWord()
     while (newWord === word) newWord = getRandomWord()
     setWord(newWord)
     setRevealedLetters(getHiddenLetters(newWord))
@@ -39,7 +39,7 @@ const Game = () => {
             return newRevealedState
           }
         })
-      }, 3000)
+      }, 10000)
       return () => clearInterval(interval)
     }
   }, [gameState])
